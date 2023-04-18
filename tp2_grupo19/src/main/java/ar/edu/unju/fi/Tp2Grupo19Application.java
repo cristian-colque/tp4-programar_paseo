b42cc1e3d3a9d67a757b4003c2681a3e48ba330c
cherange <ange@lune>
2023-04-17 22:51:36 -0300

    Creación del proyecto.

---
+++ tp2_grupo19/src/main/java/ar/edu/unju/fi/Tp2Grupo19Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2Grupo19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2Grupo19Application.class, args);
+	}
+
+}
